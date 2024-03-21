bf91674f737c175fedc6832eecb72359f41120a4
wlsdks <wlsdks12@naver.com>
2024-03-21 20:38:52 +0900

    feat: 데이터, 요청, 응답 dto객체 추가

---
+++ src/main/java/com/fastcampus/boardserver/dto/request/UserDeleteId.java
@@ -0,0 +1,17 @@
+package com.fastcampus.boardserver.dto.request;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class UserDeleteId {
+
+    @NonNull
+    private String id;
+
+    @NonNull
+    private String password;
+
+}
---
+++ src/main/java/com/fastcampus/boardserver/dto/request/UserLoginRequest.java
@@ -0,0 +1,17 @@
+package com.fastcampus.boardserver.dto.request;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class UserLoginRequest {
+
+    @NonNull
+    private String userId;
+
+    @NonNull
+    private String password;
+
+}
