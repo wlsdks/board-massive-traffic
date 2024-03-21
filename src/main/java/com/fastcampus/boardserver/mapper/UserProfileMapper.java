06ec5525b59f28b531b530dcd83637cdaec8f0d2
wlsdks <wlsdks12@naver.com>
2024-03-21 20:39:44 +0900

    feat: mybatis 매퍼 클래스 및 xml 코드 추가

---
+++ src/main/java/com/fastcampus/boardserver/mapper/UserProfileMapper.java
@@ -0,0 +1,34 @@
+package com.fastcampus.boardserver.mapper;
+
+import com.fastcampus.boardserver.dto.UserDTO;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * mybatis 매퍼 인터페이스 선언
+ */
+@Mapper
+public interface UserProfileMapper {
+
+    public UserDTO getUserProfile(@Param("id") String id);
+
+    int insertUserProfile(@Param("id") String id, @Param("password") String password, @Param("name") String name, @Param("phone") String phone, @Param("address") String address);
+
+    int updateUserProfile(@Param("id") String id, @Param("password") String password, @Param("name") String name, @Param("phone") String phone, @Param("address") String address);
+
+    int deleteUserProfile(@Param("id") String id);
+
+    public int register(UserDTO userDTO);
+
+    public UserDTO findByIdAndPassword(@Param("id") String id,
+                                       @Param("password") String password);
+
+    public UserDTO findByUserIdAndPassword(@Param("userId") String userId,
+                                           @Param("password") String password);
+
+    int idCheck(@Param("userId") String userId);
+
+    public int updatePassword(UserDTO userDTO);
+
+//    public int updateAddress(UserDTO userDTO);
+}
