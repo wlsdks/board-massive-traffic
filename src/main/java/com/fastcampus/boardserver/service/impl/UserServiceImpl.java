7cabf8d0a6e96343ccafd70932d998a6b3f8c34f
wlsdks <wlsdks12@naver.com>
2024-03-21 20:39:29 +0900

    feat: 서비스 코드 추가

---
+++ src/main/java/com/fastcampus/boardserver/service/impl/UserServiceImpl.java
@@ -0,0 +1,82 @@
+package com.fastcampus.boardserver.service.impl;
+
+import com.fastcampus.boardserver.dto.UserDTO;
+import com.fastcampus.boardserver.exception.DuplicateIdException;
+import com.fastcampus.boardserver.mapper.UserProfileMapper;
+import com.fastcampus.boardserver.service.UserService;
+import com.fastcampus.boardserver.util.SHA256Util;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@RequiredArgsConstructor
+@Log4j2
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserProfileMapper userProfileMapper;
+
+
+    @Override
+    public void register(UserDTO userDTO) {
+        boolean duplicatedIdResult = isDuplicatedId(userDTO.getUserId());
+        if (duplicatedIdResult) {
+            throw new DuplicateIdException("중복된 아이디가 존재합니다.");
+        }
+        userDTO.setCreateTime(new Date());
+        userDTO.setPassword(SHA256Util.encryptSHA256(userDTO.getPassword()));
+        int insertCount = userProfileMapper.register(userDTO);
+
+        if (insertCount != 1) {
+            log.error("register error: {}", userDTO);
+            throw new RuntimeException("insert User Error! 회원가입 메서드를 확인해주세요.\n" + "Params : " + userDTO);
+        }
+    }
+
+    @Override
+    public UserDTO login(String id, String password) {
+        String cryptoPassword = SHA256Util.encryptSHA256(password);
+        UserDTO memberInfo = userProfileMapper.findByIdAndPassword(id, cryptoPassword);
+        return memberInfo;
+    }
+
+    @Override
+    public boolean isDuplicatedId(String id) {
+        return userProfileMapper.idCheck(id) == 1;
+    }
+
+    @Override
+    public UserDTO getUserInfo(String userId) {
+        return null;
+    }
+
+    @Override
+    public void updatePassword(String id, String beforePassword, String afterPassword) {
+        String cryptoPassword = SHA256Util.encryptSHA256(beforePassword);
+        UserDTO memberInfo = userProfileMapper.findByIdAndPassword(id, cryptoPassword);
+
+        if (memberInfo != null) {
+            memberInfo.setPassword(SHA256Util.encryptSHA256(afterPassword));
+            int insertCount = userProfileMapper.updatePassword(memberInfo);
+        } else {
+            log.error("updatePassword error: {}", memberInfo);
+            throw new RuntimeException("비밀번호 변경 실패");
+        }
+    }
+
+    @Override
+    public void deleteId(String id, String password) {
+        String cryptoPassword = SHA256Util.encryptSHA256(password);
+        UserDTO memberInfo = userProfileMapper.findByIdAndPassword(id, cryptoPassword);
+
+        if (memberInfo != null) {
+            int deleteCount = userProfileMapper.deleteUserProfile(id);
+        } else {
+            log.error("deleteId error: {}", memberInfo);
+            throw new RuntimeException("회원탈퇴 실패");
+        }
+    }
+
+}
