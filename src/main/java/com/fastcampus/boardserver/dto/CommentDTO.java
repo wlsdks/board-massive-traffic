6d9636ce05d5686687d462598acf1ff213fd1660
wlsdks <wlsdks12@naver.com>
2024-03-24 18:49:37 +0900

    feat: 댓글, 태그 dto 생성

---
+++ src/main/java/com/fastcampus/boardserver/dto/CommentDTO.java
@@ -0,0 +1,18 @@
+package com.fastcampus.boardserver.dto;
+
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentDTO {
+
+    private int id;
+    private int postId;
+    private String contents;
+    private Integer subCommentId;
+
+}
