2159b8462537cf695b630d98fd0548b676dbfe5b
wlsdks <wlsdks12@naver.com>
2024-03-24 18:51:48 +0900

    feat: 태그로 게시글 조회 컨트롤러 작성

--- src/main/java/com/fastcampus/boardserver/controller/PostSearchController.java
+++ src/main/java/com/fastcampus/boardserver/controller/PostSearchController.java
@@ -7,10 +7,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,6 +22,15 @@ public class PostSearchController {
     @PostMapping
     public PostSearchResponse search(@RequestBody PostSearchRequest postSearchRequest) {
         List<PostDTO> postDTOList = postSearchService.getPosts(postSearchRequest);
+        return new PostSearchResponse(postDTOList);
+    }
+
+    /**
+     * 게시글 태그 정보로 조회
+     */
+    @GetMapping
+    public PostSearchResponse searchByTag(@RequestParam String tagName) {
+        List<PostDTO> postDTOList = postSearchService.getPostsByTag(tagName);
         return new PostSearchResponse(postDTOList);
     }
 
