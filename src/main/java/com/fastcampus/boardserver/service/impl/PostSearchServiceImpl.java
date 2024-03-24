b4637dd3ea2f8ae3b353004ed928e51a0a8c6b87
wlsdks <wlsdks12@naver.com>
2024-03-25 00:16:07 +0900

    feat: 로그 수정 및 추가

--- src/main/java/com/fastcampus/boardserver/service/impl/PostSearchServiceImpl.java
+++ src/main/java/com/fastcampus/boardserver/service/impl/PostSearchServiceImpl.java
@@ -2,11 +2,13 @@ package com.fastcampus.boardserver.service.impl;
 
 import com.fastcampus.boardserver.dto.PostDTO;
 import com.fastcampus.boardserver.dto.request.PostSearchRequest;
+import com.fastcampus.boardserver.exception.BoardServerException;
 import com.fastcampus.boardserver.mapper.PostSearchMapper;
 import com.fastcampus.boardserver.service.PostSearchService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.HttpStatus;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -29,8 +31,9 @@ public class PostSearchServiceImpl implements PostSearchService {
         List<PostDTO> postDTOList = null;
         try {
             postDTOList = postSearchMapper.selectPosts(postSearchRequest);
-        } catch (Exception e) {
-            log.error("selectPosts 메서드 실패", e.getMessage());
+        } catch (RuntimeException e) {
+            log.error("getPosts 실패");
+            throw new BoardServerException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
         }
         return postDTOList;
     }
